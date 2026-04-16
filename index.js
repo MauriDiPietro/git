eed8f1b4ce3ae9823bac53a0727c493d0f451c34
Mauri Di Pietro <dipietro.jm@gmail.com>
2026-04-16 19:47:42 -0300

    Feat/log (#5)
    * fhfghfh
    
    * sasd
    
    * cambio nuevo

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const variable1 = 'asdasdasd';
 console.log(`¡${variable1}!`);
 
 console.log(`El resultado de la suma es: ${resultado}`);
+const resultado = 2 + 3;rtyrtyrtytry
+
+console.log(`El resultado de la suma es: ${resultado}`);
+rtyrtyrtyrtyrty
 const resultado = 2 + 3;
 
 console.log(`El resultado de la suma es: ${resultado}`);
