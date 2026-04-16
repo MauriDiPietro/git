ab94d33b744589f94ea06d708d6a5473af9094c3
MauriDiPietro <dipietro.jm@gmail.com>
2026-04-16 19:46:19 -0300

    cambio nuevo

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const variable1 = 'asdasdasd';
 console.log(`¡${variable1}!`);
 
 console.log(`El resultado de la suma es: ${resultado}`);
-const resultado = 2 + 3;
+const resultado = 2 + 3;rtyrtyrtytry
 
 console.log(`El resultado de la suma es: ${resultado}`);
+rtyrtyrtyrtyrty
